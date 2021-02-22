402f924084d1e2cb5b43498e47a7f5ced099fee1
Carlo Messere <messerecarlo27@gmail.com>
2021-02-22 22:26:48 +0100

    added switch and home skeleton component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div >
+      <Switch>
+        <Route exact path="/"><Home/></Route>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+const Home = () => {
+  return(
+    <h1>Home</h1>
+  )
+}
