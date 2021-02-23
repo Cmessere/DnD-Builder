92ca02d62fc3dbba0febc9cfa38e096e96598656
Carlo Messere <messerecarlo27@gmail.com>
2021-02-23 21:48:59 +0100

    removed warning

--- src/components/MonsterGrid.tsx
+++ src/components/MonsterGrid.tsx
@@ -7,19 +7,19 @@ export const MonsterGrid = ({ monsters }: MonsterGridProps) => {
     return (
         <div className="monsters-list-div">
             <div className="monsters-row">
-                {monsters.slice(0, 5).map((monster) => <MonsterCard monster={monster} />)}
+                {monsters.slice(0, 5).map((monster) => <li className="monster-list-item" key={monster.name}><MonsterCard monster={monster}/></li>)}
             </div>
             <div className="monsters-row">
-                {monsters.slice(5, 10).map((monster) => <MonsterCard monster={monster} />)}
+                {monsters.slice(5, 10).map((monster) => <li className="monster-list-item" key={monster.name}><MonsterCard monster={monster}/></li>)}
             </div>
             <div className="monsters-row">
-                {monsters.slice(10, 15).map((monster) => <MonsterCard monster={monster} />)}
+                {monsters.slice(10, 15).map((monster) => <li className="monster-list-item" key={monster.name}><MonsterCard monster={monster}/></li>)}
             </div>
             <div className="monsters-row">
-                {monsters.slice(15, 20).map((monster) => <MonsterCard monster={monster} />)}
+                {monsters.slice(15, 20).map((monster) => <li className="monster-list-item" key={monster.name}><MonsterCard monster={monster}/></li>)}
             </div>
             <div className="monsters-row">
-                {monsters.slice(20, 25).map((monster) => <MonsterCard monster={monster} />)}
+                {monsters.slice(20, 25).map((monster) => <li className="monster-list-item" key={monster.name}><MonsterCard monster={monster}/></li>)}
             </div>
         </div>
     );
