6f3e6e17eb5f45ba2cb4e9626bbd76988b9bd5ef
Carlo Messere <messerecarlo27@gmail.com>
2021-02-23 19:01:55 +0100

    Moved Home in own file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Monsters } from '../services/ApiClient';
-import { MonsterList } from '../services/types';
+import { Home } from './Home';
 
 function App() {
   return (
@@ -14,28 +13,3 @@ function App() {
 }
 
 export default App;
-
-const Home = () => {
-  const [monstersList, setMonstersList] = React.useState(undefined as any as MonsterList)
-
-  React.useEffect(() => {
-    Monsters()
-    .then((response) => setMonstersList(response.data))
-    .catch(error => console.log("error :",error))
-  }, [])
-
-  if(monstersList){
-    return(
-      <div className="home-page"> 
-        <h1>Monsters List</h1>
-        <h2>Total count: {monstersList.count}</h2>
-        {monstersList.results.map((monster) => <p>{monster.name}</p>)}
-      </div>
-    )
-  }
-  else{
-    return(
-      <h1>Loading</h1>
-    )
-  }
-}
---
+++ src/components/Home.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Monsters } from '../services/ApiClient';
+import { MonsterList } from '../services/types';
+
+export const Home = () => {
+  const [monstersList, setMonstersList] = React.useState(undefined as any as MonsterList);
+
+  React.useEffect(() => {
+    Monsters()
+      .then((response) => setMonstersList(response.data))
+      .catch(error => console.log("error :", error));
+  }, []);
+
+  if (monstersList) {
+    return (
+      <div className="home-page">
+        <h1>Monsters List</h1>
+        <h2>Total count: {monstersList.count}</h2>
+        {monstersList.results.map((monster) => <p>{monster.name}</p>)}
+      </div>
+    );
+  }
+  else {
+    return (
+      <h1>Loading</h1>
+    );
+  }
+};
