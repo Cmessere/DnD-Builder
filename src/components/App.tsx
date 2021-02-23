20b85ffcd3706b4cee28d6b6971189ab988aab0d
Carlo Messere <messerecarlo27@gmail.com>
2021-02-23 19:04:31 +0100

    Formatted documents

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import { Home } from './Home';
 
 function App() {
-  return (
-    <div >
-      <Switch>
-        <Route exact path="/"><Home/></Route>
-      </Switch>
-    </div>
-  );
+    return (
+        <div >
+            <Switch>
+                <Route exact path="/"><Home /></Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,24 +4,24 @@ import { MonsterList } from '../services/types';
 import { Loading } from './Loading';
 
 export const Home = () => {
-  const [monstersList, setMonstersList] = React.useState(undefined as any as MonsterList);
+    const [monstersList, setMonstersList] = React.useState(undefined as any as MonsterList);
 
-  React.useEffect(() => {
-    Monsters()
-      .then((response) => setMonstersList(response.data))
-      .catch(error => console.log("error :", error));
-  }, []);
+    React.useEffect(() => {
+        Monsters()
+            .then((response) => setMonstersList(response.data))
+            .catch(error => console.log("error :", error));
+    }, []);
 
-  if (monstersList) {
-    return (
-      <div className="home-page">
-        <h1>Monsters List</h1>
-        <h2>Total count: {monstersList.count}</h2>
-        {monstersList.results.map((monster) => <p>{monster.name}</p>)}
-      </div>
-    );
-  }
-  else {
-    return <Loading />
-  }
+    if (monstersList) {
+        return (
+            <div className="home-page">
+                <h1>Monsters List</h1>
+                <h2>Total count: {monstersList.count}</h2>
+                {monstersList.results.map((monster) => <p>{monster.name}</p>)}
+            </div>
+        );
+    }
+    else {
+        return <Loading />
+    }
 };
