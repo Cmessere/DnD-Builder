bb09d7f8f769a7a8d433877f65d719c8642afb31
Carlo Messere <messerecarlo27@gmail.com>
2021-03-01 01:27:36 +0100

    calculating Proficiency bonus

--- src/components/AdditionalStats.tsx
+++ src/components/AdditionalStats.tsx
@@ -5,6 +5,7 @@ import "./styles/Detail.css"
 
 export const AdditionalStats = ({ senses, languages, cr, xp }: AdditionalStatsProps) => {
     const prettySenses = prettifySenses(senses);
+    const proficiencyBonus = calculateProficiencyBonus(cr)
     return (
         <div>
             <div className="string-div">
@@ -18,7 +19,7 @@ export const AdditionalStats = ({ senses, languages, cr, xp }: AdditionalStatsPr
                     <p><b>Challenge </b>{cr} ({xp} XP) </p>
                 </div>
                 <div>
-                    <p><b>Proficiency Bonus </b> </p>
+                    <p><b>Proficiency Bonus </b> +{proficiencyBonus}</p>
                 </div>
             </div>
         </div>
@@ -37,3 +38,24 @@ const prettifySenses = (senses: any[]) => {
 function capitalizaAndRemoveHyphen(label: string) {
     return label.split("_").map(s => capitalize(s)).join(" ");
 }
+
+const calculateProficiencyBonus = (difficultyRating:number) => {
+    switch(true){
+        case (difficultyRating >= 0 && difficultyRating < 5):
+            return 2
+        case (difficultyRating >= 5 && difficultyRating < 9):
+            return 3
+        case (difficultyRating >= 9 && difficultyRating < 13):
+            return 4
+        case (difficultyRating >= 13 && difficultyRating < 17):
+            return 5
+        case (difficultyRating >= 17 && difficultyRating < 21):
+            return 6
+        case (difficultyRating >= 21 && difficultyRating < 25):
+            return 7
+        case (difficultyRating >= 25 && difficultyRating < 29):
+            return 8
+        case (difficultyRating >= 29 && difficultyRating < 30):
+            return 9
+    }
+} 
