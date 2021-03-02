b1ebfb486f5e9ba7542bca3514a2bb8f9a84f7c5
Carlo Messere <messerecarlo27@gmail.com>
2021-03-02 22:38:30 +0100

    general Layout tweak

--- src/components/SpecialAbilitiesComponent.tsx
+++ src/components/SpecialAbilitiesComponent.tsx
@@ -14,7 +14,7 @@ const RenderSpecialAbility = ({ name, desc, usage }: SpecialAbilities) => {
     return (
         <div>
             {usage && <><Title label={<>{name} ({usage.times}/{usage.type}):</>}/><Typography variant="subtitle1">{desc}</Typography></>}
-            {!usage && <><Title label={<>{name}:</>} /><Typography variant="subtitle1">{desc}</Typography></>}
+            {!usage && <><Title label={<>{name}:</>} /><Typography style={{display:"flex",flexWrap: "wrap"}} variant="subtitle1">{desc}</Typography></>}
         </div>
     );
 };
