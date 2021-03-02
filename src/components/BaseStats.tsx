2c76d1c914a28e18c84f4a5578c3a49615aeadd0
Carlo Messere <messerecarlo27@gmail.com>
2021-03-02 01:53:48 +0100

    styled base stats

--- src/components/BaseStats.tsx
+++ src/components/BaseStats.tsx
@@ -1,5 +1,9 @@
+import Typography from "@material-ui/core/Typography";
 import React from "react";
 import { BaseStatsProps } from "../services/types";
+import { Subtitle } from "../utilities/Subtitle";
+import { IconButton } from "@material-ui/core";
+import CasinoIcon from '@material-ui/icons/Casino';
 
 export const BaseStats = ({ AC, HP, hitDice, speed, constitutionModifier }: BaseStatsProps) => {
 
@@ -14,12 +18,26 @@ export const BaseStats = ({ AC, HP, hitDice, speed, constitutionModifier }: Base
 
     return (
         <div className="Monster-Sheet-BaseStats">
-            <p><b>Armor Class</b> {AC}</p>
-            <p><b>Hit Points</b> {HP} {hitDice} <button onClick={rollHitDice}>Randomize</button> {randomHP}</p>
-            <p><b>Speed</b> {speedArray.map(value => value[0] + " " + value[1])}</p>
+            <div className="Monster-Sheet-div">
+                <Subtitle label={<b>Armor Class</b>} />
+                <Typography style={{ marginLeft: "1vw" }} variant="subtitle2">{AC}</Typography>
+            </div>
+            <div className="Monster-Sheet-div">
+                <Subtitle label={<b>Hit Points</b>} />
+                <Typography style={{ marginLeft: "1vw" }} variant="subtitle2">{HP} {hitDice}</Typography>
+                <IconButton color="secondary" aria-label="roll-hit-dices" onClick={rollHitDice}>
+                    <CasinoIcon />
+                </IconButton>
+                <Typography color="secondary" style={{ marginLeft: "1vw" }} variant="subtitle2"> {randomHP}</Typography>
+            </div>
+            <div className="Monster-Sheet-div">
+                <Subtitle label={<b>Speed</b>} />
+                <Typography style={{ marginLeft: "1vw" }} variant="subtitle2">{speedArray.map(value => value[0] + " " + value[1])}</Typography>
+            </div>
         </div>
     );
 };
+
 const calculateRandomHP = (rolls: number, dice: number, modifier: number): number => {
     let count = 1;
     for (let i = 0; i < rolls; i++) {
---
+++ src/utilities/Subtitle.tsx
@@ -0,0 +1,6 @@
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+
+export const Subtitle = ({ label }: any) => {
+    return <Typography color="primary" variant="subtitle1">{label}</Typography>;
+};
