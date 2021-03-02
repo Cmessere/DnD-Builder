0c77adce367f60d3d45578dc77661c3079aeb5bf
Carlo Messere <messerecarlo27@gmail.com>
2021-03-02 23:25:50 +0100

    Added abilities throws on stats

--- src/components/AbilityPoints.tsx
+++ src/components/AbilityPoints.tsx
@@ -11,20 +11,26 @@ export const AbilityPoints = ({ str, dex, con, int, wis, cha }: AbilityPointsPro
     return (
         <>
         <div className="Ability-Points" >
-            {abilities.map(ability => <div className="Ability-Points-div" key={ability[0]}><AbilityLabel label={ability[0]} value={ability[1]} /></div>)}
+            {abilities.map(ability => <div  className="Ability-Points-div" key={ability[0]}><AbilityLabel label={ability[0]} value={ability[1]} /></div>)}
         </div>
         <Divider/>
         </>
     );
 };
+
 const AbilityLabel = ({ label, value }: any) => {
     const modifier = Math.floor((value - 10) / 2);
-
+    const [rolledStat, setRolledStat] = React.useState(undefined as unknown as number)
     return (
-        <div className="Abilities-div">
-            <Typography variant="subtitle1" className="Abilities-title"><b>{label}</b></Typography>
-            {modifier > 0 &&  <Typography color="secondary" variant="caption" className="Abilities-paragraph">{value} (+{modifier})</Typography>}
-            {modifier <= 0 &&  <Typography color="secondary" variant="caption" className="Abilities-paragraph">{value} ({modifier})</Typography>}
+        <div className="Abilities-div" onClick={() => rollStat(modifier, setRolledStat)}>
+            <Typography color="primary" variant="subtitle1" className="Abilities-title"><b>{label}</b></Typography>
+            {modifier > 0 &&  <Typography variant="caption" className="Abilities-paragraph">{value} (+{modifier})</Typography>}
+            {modifier <= 0 &&  <Typography variant="caption" className="Abilities-paragraph">{value} ({modifier})</Typography>}
+            {rolledStat && <Typography color="secondary" variant="caption" className="Abilities-paragraph">{rolledStat}</Typography>}
         </div>
     );
 };
+
+const rollStat = (modifier:number, setRolledStat:React.Dispatch<React.SetStateAction<number>>) =>{
+    setRolledStat(Math.ceil((Math.random() * 20 +1) + modifier))
+}
