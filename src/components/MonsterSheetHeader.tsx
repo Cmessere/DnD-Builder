73884a0c8bea397c73077283a052cc4b0fb3e9ef
Carlo Messere <messerecarlo27@gmail.com>
2021-03-02 01:13:18 +0100

    Using typography in MonsterSheetHeader

--- src/components/MonsterSheetHeader.tsx
+++ src/components/MonsterSheetHeader.tsx
@@ -1,17 +1,18 @@
+import { Typography } from "@material-ui/core";
 import { MonsterSheetHeaderProps } from "../services/types";
 
 export const MonsterSheetHeader = ({ name, size, type, subtype, alignment }: MonsterSheetHeaderProps) => {
     
     const generateParagraph = () => {
         if(subtype) 
-            return (<p className="Monster-sheet-description-paragraph">{size} {type} ({subtype}), {alignment}</p>)
+            return (<Typography variant="subtitle2" className="Monster-sheet-description-paragraph">{size} {type} ({subtype}), {alignment}</Typography>)
         else 
-            return (<p className="Monster-sheet-description-paragraph">{size} {type}, {alignment}</p>)
+            return (<Typography variant="subtitle2" className="Monster-sheet-description-paragraph">{size} {type}, {alignment}</Typography>)
     }
     
     return (
         <div className="Monster-Sheet-header">
-            <h1>{name}</h1>
+            <Typography variant="h3" color="primary">{name}</Typography>
             <div className="Monster-sheet-description">
                 {generateParagraph()}
             </div>
